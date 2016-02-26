2c7d363f016c454f53ffcc3c388f09832f56100d
Maciej Smolinski <contact@maciejsmolinski.com>
2016-02-26 16:57:44 +0100

    Errors: Give a better message when module's not found.

--- webpack/js/index.js
+++ webpack/js/index.js
@@ -2,9 +2,33 @@ var Elm = require('../elm/Main.elm');
 var css = require('../css/style.scss');
 
 var helpers = {
+
   toArray: function (data) {
     return [].slice.call(data);
   },
+
+  buildSelector: function buildSelector(element) {
+    var selector      = '';
+    var parentElement = element.parentElement;
+    var classList     = helpers.toArray(element.classList);
+
+    if (parentElement) {
+      selector += buildSelector(parentElement) + ' ';
+    }
+
+    selector += element.tagName.toLowerCase();
+
+    if (element.id) {
+      selector += '#' + element.id;
+    }
+
+    if (classList.length) {
+      selector += '.' + classList.join('.');
+    }
+
+    return selector;
+  },
+
 };
 
 helpers.toArray(
@@ -21,7 +45,9 @@ helpers.toArray(
 .forEach(function (config) {
   if (!(config.module in Elm)) {
     throw new Error(
-      '[Elm] Module "%s" not found'.replace('%s', config.module)
+      '[Elm] Module "%s" bound to "%s" not found in Elm namespace'
+      .replace('%s', config.module)
+      .replace('%s', helpers.buildSelector(config.element))
     );
   }
 
