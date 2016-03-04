a1e8e4bcd1cb97281ec5acb0d7f5d9c7b8c9bc65
Maciej Smolinski <contact@maciejsmolinski.com>
2016-03-04 08:38:57 +0100

    RandomUser: Display random user email every 5 seconds.

--- randomuser/elm/Main.elm
+++ randomuser/elm/Main.elm
@@ -3,52 +3,58 @@ module Main where
 import Http
 import Json.Decode as Json exposing ((:=))
 import Task exposing (Task)
-import Graphics.Element exposing (show, Element)
+import Html exposing (text, Html)
 import Signal
+import Time
+import String
 
 type alias User =
-  { email: String
+  { emails: List String
   }
 
 type Action = NoOp
-            | UpdateEmail String
+            | UpdateEmails (List String)
 
 mailbox: Signal.Mailbox Action
 mailbox = Signal.mailbox NoOp
 
 modelUpdate: Action -> User -> User
 modelUpdate action model =
-  case action of
-    UpdateEmail email ->
-      { model | email = email }
+  case Debug.log "Action" action of
+    UpdateEmails emails ->
+      { model | emails = emails }
     _ ->
       model
 
 modelSignal: Signal User
 modelSignal =
-  Signal.foldp modelUpdate (User "") mailbox.signal
+  Signal.foldp modelUpdate (User [""]) mailbox.signal
 
-view: User -> Element
+view: User -> Html
 view model =
-  show model.email
+  text
+    <| String.join ", " (model.emails)
 
-main: Signal Element
+main: Signal Html
 main =
   Signal.map view modelSignal
 
 
-getData: Task Http.Error String
+getData: Task Http.Error (List String)
 getData =
-  Http.get getEmail "https://randomuser.me/api/"
---
-getEmail: Json.Decoder String
-getEmail =
-  Json.at ["results", "user", "email"] Json.string
---
--- handleData: String -> Signal.Address Action
--- handleData email =
---   Signal.forwardTo mailbox.address (\email -> UpdateEmail email)
---
--- port run: Task Http.Error ()
--- port run =
---   Task.andThen getData handleData
+  Http.get getEmails "https://randomuser.me/api/"
+
+getEmails: Json.Decoder (List String)
+getEmails =
+  let
+    email = Json.at ["user", "email"] Json.string
+  in
+    ("results" := Json.list email)
+
+handleData: List String -> Task a ()
+handleData email =
+    Signal.send mailbox.address (UpdateEmails email)
+
+port runner2: Signal (Task Http.Error ())
+port runner2 =
+  Signal.map (always (Task.andThen getData handleData)) (Time.every (5 * Time.second))
