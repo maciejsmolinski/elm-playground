269781fe0fa8ac286cc62184c3a2b76c68dcece1
Maciej Smolinski <contact@maciejsmolinski.com>
2016-03-16 20:59:19 +0100

    MatchEvents: Send `EventSource` data to Elm app.

--- match-events/js/index.js
+++ match-events/js/index.js
@@ -1,7 +1,42 @@
 var Elm = require('../elm/Main.elm');
 var css = require('../css/style.scss');
 
+var local = {
+  Main: function (app) {
+    var sse;
+
+    console.log('[Local] App Main bound');
+
+    sse =
+      helpers.sse('http://localhost:7654/events');
+
+    sse.onmessage =
+      function (event) {
+        var message =
+          helpers.toJSON(event.data);
+
+        console.log('[SSE] Data received %o', message);
+
+        app.ports.events.send(message.label);
+
+        return message;
+      };
+  },
+};
+
 var helpers = {
+
+  sse: function (endpoint) {
+    return new EventSource(endpoint);
+  },
+
+  toJSON: function (string) {
+    try {
+      return JSON.parse(string);
+    } catch (_) {
+      return {};
+    }
+  },
 
   toArray: function (data) {
     return [].slice.call(data);
@@ -51,8 +86,14 @@ helpers.toArray(
     );
   }
 
-  Elm.embed(
+  var app = Elm.embed(
     Elm[config.module],
-    config.element
+    config.element,
+    { events: '' }
   );
+
+  // Once App bound to element, init local js interop
+  if (local[config.module]) {
+    local[config.module](app);
+  }
 });
