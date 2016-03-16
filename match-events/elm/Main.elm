b8ef0ec7ddc374bc4052bc9ff00a5501951f7869
Maciej Smolinski <contact@maciejsmolinski.com>
2016-03-16 20:58:46 +0100

    MatchEvents: Take events from ports and mailbox, display events below the pitch.

--- match-events/elm/Main.elm
+++ match-events/elm/Main.elm
@@ -1,9 +1,65 @@
 module Main where
 
 import Html exposing (text, div)
+import Signal
+import Html exposing (Html)
 
-main : Html.Html
+type alias Event =
+  String
+
+type alias Events =
+  List Event
+
+type alias Model =
+  { events: Events
+  }
+
+type Action
+  = NoOp
+  | AddEvent String
+
+mailbox: Signal.Mailbox Action
+mailbox = Signal.mailbox NoOp
+
+initialModel : Model
+initialModel =
+  Model []
+
+signalModel : Signal Model
+signalModel =
+  let
+    portSignal =
+      Signal.map AddEvent events
+
+    mailboxSignal =
+      mailbox.signal
+
+    signals =
+      Signal.merge portSignal mailboxSignal
+  in
+    Signal.foldp updateModel initialModel signals
+
+updateModel : Action -> Model -> Model
+updateModel action model =
+  case action of
+    AddEvent eventName ->
+      { model | events = model.events ++ [ eventName ] }
+
+    NoOp ->
+      model
+
+main : Signal Html
 main =
-  div
-    []
-    [ text "Match Events" ]
+  Signal.map view signalModel
+
+view : Model -> Html
+view model =
+  let
+    events =
+      List.map (\name -> div [] [ text name ]) model.events
+  in
+    div
+      []
+      events
+
+port events : Signal Event
