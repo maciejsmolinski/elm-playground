398ff94a850b97183955a745b8ab07da8a20a46b
Maciej Smolinski <contact@maciejsmolinski.com>
2016-06-01 22:07:38 +0200

    SpotifyMini: Display message about empty search results.

--- spotify-mini/elm/TrackList/View.elm
+++ spotify-mini/elm/TrackList/View.elm
@@ -10,7 +10,10 @@ render : TrackList -> Html Action
 render trackList =
     let
         tracksElements =
-            List.map Track.View.render trackList
+            if List.length trackList > 0 then
+                List.map Track.View.render trackList
+            else
+                [ div [] [ text "Empty search results" ] ]
     in
         div []
             tracksElements
