395b3b9c082e15c4eb1b416d2d0e69767e451e96
Maciej Smolinski <contact@maciejsmolinski.com>
2016-06-27 19:55:47 +0200

    Navigation: Set default route and redirect when not matched.

--- spotify-mini/elm/Routing/App.elm
+++ spotify-mini/elm/Routing/App.elm
@@ -5,11 +5,28 @@ import App.Msg as App exposing (Msg)
 import Navigation
 
 
+{-
+   Custom URL Parser
+
+   Transforms location to something Application's init and urlUpdate functions understand, e.g. "/app", 0 or even AppHome
+   Sample Scenario:
+
+     Given location "localhost/index#/app"
+     When parsed by a customly defined parser
+     Then can be turned into "/app"
+-}
+
+
 urlParser : Navigation.Parser String
 urlParser =
-    Navigation.makeParser (\location -> "/")
+    Navigation.makeParser (\location -> location.hash)
 
 
 mapUrl : String -> App -> ( App, Cmd App.Msg )
 mapUrl uri model =
-    ( model, Cmd.none )
+    case uri of
+        "#/" ->
+            ( model, Cmd.none )
+
+        _ ->
+            ( model, Navigation.modifyUrl ("#/") )
