ef33482acedc1316750dba48f89820ffdc5fc76b
Maciej Smolinski <contact@maciejsmolinski.com>
2016-06-27 19:34:39 +0200

    Navigation: Plug in Elm Navigation.

--- spotify-mini/elm/Main.elm
+++ spotify-mini/elm/Main.elm
@@ -1,18 +1,20 @@
 module Main exposing (main)
 
-import Html.App
 import App.Model exposing (App)
 import App.View
 import App.Update
 import App.Msg as App exposing (Msg(..))
 import TrackList.Msg exposing (Msg(..))
 import Ports.Ports exposing (updateTrackList, clearTrackList)
+import Routing.App exposing (urlParser, mapUrl)
+import Navigation
 
 
 main : Program Never
 main =
-    Html.App.program
-        { init = App.Model.initial
+    Navigation.program urlParser
+        { init = (\uri -> mapUrl uri (fst App.Model.initial))
+        , urlUpdate = mapUrl
         , view = App.View.render
         , update = App.Update.update
         , subscriptions = subscriptions
