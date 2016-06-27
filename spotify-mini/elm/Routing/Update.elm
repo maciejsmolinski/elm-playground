b7b5cb0389904451fd3178a9d524141a4f0245f5
Maciej Smolinski <contact@maciejsmolinski.com>
2016-06-27 20:38:31 +0200

    Format: Format code.

--- spotify-mini/elm/Routing/Update.elm
+++ spotify-mini/elm/Routing/Update.elm
@@ -4,6 +4,7 @@ import Routing.Msg exposing (Msg(..))
 import Routing.Model exposing (Routing)
 import Navigation
 
+
 update : Msg -> Routing -> ( Routing, Cmd Msg )
 update msg model =
     case msg of
@@ -11,7 +12,7 @@ update msg model =
             ( { model | page = Just "home" }, Cmd.none )
 
         NotFound ->
-            ( { model | page = Just "not-found" }, Cmd.none)
+            ( { model | page = Just "not-found" }, Cmd.none )
 
         Redirect uri ->
             ( { model | page = Nothing }, Navigation.modifyUrl uri )
--- spotify-mini/elm/Track/Msg.elm
+++ spotify-mini/elm/Track/Msg.elm
@@ -2,6 +2,7 @@ module Track.Msg exposing (Msg(..))
 
 import Track.Model exposing (Track)
 
+
 type Msg
     = NoOp
     | PlayTrack Track
